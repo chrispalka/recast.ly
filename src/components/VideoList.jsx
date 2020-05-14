dd587de3f934ea0f5d78c8b9cac63e44c5d23925
Chris Palka <cpalka87gmail.com>
2020-05-13 20:04:25 -0600

    Complete creating a dynamic Video List component

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,12 +1,18 @@
+import VideoListEntry from '../components/VideoListEntry.js';
+
 var VideoList = (props) => (
   <div className="video-list">
-    <div><h5><em>{props.videoData}</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    {props.videos.map((video, index) =>
+      <VideoListEntry video={video} key={index} />
+    )}
   </div>
 );
+
+/*
+{props.videos.map(video =>
+  <VideoListEntry video={video} />
+)}
+*/
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 // TODO: Render the `App` component to the DOM
 // Import List
 import App from './components/App.js';
-import VideoList from './components/VideoList.js';
-import exampleVideoData from './data/exampleVideoData.js';
+// import VideoList from './components/VideoList.js';
+// import exampleVideoData from './data/exampleVideoData.js';
 
 
 // call ReactDOM.render, passing in App
 ReactDOM.render(<App />, document.getElementById('app'));
 
 // RenderVideo List
-ReactDOM.render(<VideoList videoData={exampleVideoData}/>, document.getElementById('videoList'));
+// ReactDOM.render(<VideoList videos={exampleVideoData}/>, document.getElementById('videoList'));
 
 // <VideoList videoListItems = {[exampleVideoData[0], 1, 2, 3]} />,
 //[...VideoList] = [10, 20, 30, 40, 50];
